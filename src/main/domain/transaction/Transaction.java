4dd9ebcabdb7c308393c2578c4590ea707836332
Peziiim <eduardoorx@gmail.com>
2025-04-21 21:30:54 -0300

    feat: created Transaction Entity

---
+++ src/main/domain/transaction/Transaction.java
@@ -0,0 +1,88 @@
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.ManyToAny;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.EqualsAndHashCode;
+
+@Entity(name="transactions")
+@Table(name="transactions")
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal amount;
+    
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private User sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private User receiver;
+
+
+    private LocalDateTime timeStamp;
+
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+
+    public User getSender() {
+        return sender;
+    }
+
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    
+}
