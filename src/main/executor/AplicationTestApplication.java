f371da4a4ec50df6d7d8483009d776badb95d37a
Peziiim <eduardoorx@gmail.com>
2025-04-21 21:29:39 -0300

    init commit

---
+++ src/main/executor/AplicationTestApplication.java
@@ -0,0 +1,13 @@
+package com.demo.aplication_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicationTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationTestApplication.class, args);
+	}
+
+}
